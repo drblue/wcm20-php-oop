9c3a0aed2550daca623d0f2ade1ac593dd0c4d02
Johan Nordström <johan@digitalvillage.se>
2021-02-17 10:24:50 +0100

    Solution example workshop 2 (unordered list of properties).

--- 01-basic-classes/workshop/includes/Horse.php
+++ 01-basic-classes/workshop/includes/Horse.php
@@ -4,17 +4,20 @@ class Horse {
 	public $name = 'Anonymous Horse';
 	public $breed = "mixed";
 	public $color = "grey";
-	public $height;
-	public $weight;
+	public $height = 'n/a';
+	public $weight = 'n/a';
 	public $sex = "unknown";
 
 	public function getInfo() {
-		$info = "{$this->name} är en {$this->color} {$this->sex} av rasen {$this->breed}.";
-
-		if ($this->height && $this->weight) {
-			$info .= " Hen är {$this->height} cm hög och väger {$this->weight} kg.";
-		}
-
-		return $info;
+		return "
+			<h2>{$this->name}</h2>
+			<ul>
+				<li>Breed: {$this->breed}</li>
+				<li>Color: {$this->color}</li>
+				<li>Height: {$this->height} cm</li>
+				<li>Weight: {$this->weight} kg</li>
+				<li>Sex: {$this->sex}</li>
+			</ul>
+		";
 	}
 }
