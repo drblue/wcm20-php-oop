4a2ec5fef724ee2ec9e2abd593b1107ea6a8898b
Johan Nordström <johan@digitalvillage.se>
2021-02-17 12:09:32 +0100

    Getters and setters.

--- 01-basic-classes/workshop/includes/Horse.php
+++ 01-basic-classes/workshop/includes/Horse.php
@@ -1,9 +1,9 @@
 <?php
 
 class Horse {
-	public $name = 'Anonymous Horse';
-	public $breed = "mixed";
-	public $color = "grey";
+	public $name;
+	public $breed;
+	public $color;
 	public $height = 'n/a';
 	public $weight = 'n/a';
 	public $sex = "unknown";
@@ -20,24 +20,52 @@ class Horse {
 	}
 
 	public function getInfo() {
-		return "
-			<h2>{$this->name}</h2>
-			<p>Owner: {$this->getOwner()}</p>
-			<ul>
-				<li>Breed: {$this->breed}</li>
-				<li>Color: {$this->color}</li>
-				<li>Height: {$this->height} cm</li>
-				<li>Weight: {$this->weight} kg</li>
-				<li>Sex: {$this->sex}</li>
-			</ul>
-		";
+		$info = "<h2>{$this->name}</h2>";
+
+		if ($this->hasOwner()) {
+			$info .= "<p>Owner: {$this->getOwner()}</p>";
+		} else {
+			$info .= "<p>This horse is for sale! <strong>BUY NAOW!!</strong></p>";
+		}
+
+		$info .= "<ul>
+			<li>Breed: {$this->breed}</li>
+			<li>Color: {$this->color}</li>
+			<li>Height: {$this->height} cm</li>
+			<li>Weight: {$this->weight} kg</li>
+			<li>Sex: {$this->sex}</li>
+		</ul>";
+
+		return $info;
+	}
+
+	public function getHeight() {
+		return $this->height;
+	}
+
+	public function setHeight($height) {
+		if (!is_numeric($height) || $height <= 0) {
+			return false;
+		}
+
+		$this->height = $height;
+		return true;
 	}
 
 	public function getOwner() {
 		return $this->owner;
 	}
 
+	public function hasOwner() {
+		return (!empty($this->owner)); // will return TRUE if owner is NOT empty
+	}
+
 	public function setOwner($name) {
+		if (empty($name)) {
+			return false;
+		}
+
 		$this->owner = $name;
+		return true;
 	}
 }
--- 01-basic-classes/workshop/stable.php
+++ 01-basic-classes/workshop/stable.php
@@ -7,14 +7,25 @@ echo "<h1>Stallet</h1>";
 $horses = [];
 
 $horse = new Horse("Pelle", "Ardenner", "rosa"); // Horse#1, will execute Horse->__construct()
-$horse->height = 170;
+$horse->name = "Pelle";
+$horse->breed = "Ardenner";
+$horse->color = "rosa";
+$horse->setHeight(170);
 $horse->weight = 650;
 $horse->sex = "vallak";
-$horse->setOwner("Kajsa");
+
+$owner = "Kajsa";
+if ($horse->setOwner($owner)) {
+	echo "Successfully set '{$owner}' as owner to Pelle.<br>";
+} else {
+	echo "Refused to set '{$owner}' as owner to Pelle.<br>";
+}
+
 array_push($horses, $horse);
 
 $horse = new Horse("Kalle", "C-ponny", "turkos"); // #2
 $horse->sex = "hingst";
+$horse->setHeight("smol");
 $horse->setOwner("Pluto");
 array_push($horses, $horse);
 
