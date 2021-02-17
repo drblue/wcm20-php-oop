b285f922237c13d1c5d6a8f5539bffc8f150d792
Johan Nordström <johan@digitalvillage.se>
2021-02-17 09:53:31 +0100

    Solution example workshop 2 (with default values and conditional statements).

--- 01-basic-classes/workshop/includes/Horse.php
+++ 01-basic-classes/workshop/includes/Horse.php
@@ -2,13 +2,19 @@
 
 class Horse {
 	public $name = 'Anonymous Horse';
-	public $breed = 'N/A';
-	public $color = 'N/A';
-	public $height = 'N/A';
-	public $weight = 'N/A';
-	public $sex = 'N/A';
+	public $breed = "mixed";
+	public $color = "grey";
+	public $height;
+	public $weight;
+	public $sex = "unknown";
 
 	public function getInfo() {
-		return "{$this->name} är en {$this->color} {$this->sex} av rasen {$this->breed}. Hen är {$this->height} cm hög och väger {$this->weight} kg.";
+		$info = "{$this->name} är en {$this->color} {$this->sex} av rasen {$this->breed}.";
+
+		if ($this->height && $this->weight) {
+			$info .= " Hen är {$this->height} cm hög och väger {$this->weight} kg.";
+		}
+
+		return $info;
 	}
 }
