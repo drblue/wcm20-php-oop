8dfb6901fd8086387d3abb49ce8e7d9db27ac785
Johan Nordström <johan@digitalvillage.se>
2021-02-25 13:02:26 +0100

    Boilerplate for boilerplate (😛) for PDO with PHP.

---
+++ 03-bank/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include('partials/header.php');
+
+?>
+
+<h1>Welcome to Bank</h1>
+
+<h2 class="display-1">🤑</h2>
+
+<table class="table table-striped">
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>Name</th>
+			<th>Accounts</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<!-- here be content -->
+	</tbody>
+</table>
+
+<?php
+include('partials/footer.php');
