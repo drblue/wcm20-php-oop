81acc369894ce09bf942d46f6bd8daf26e3742c3
Johan Nordström <johan@digitalvillage.se>
2021-03-05 11:18:38 +0100

    Added Eloquent ORM library and configuration.

--- 04-bank/core/init.php
+++ 04-bank/core/init.php
@@ -3,14 +3,15 @@
  * Initialize app
  */
 
+// register autoloading of classes
+require_once('vendor/autoload.php');
+
 // config
 require_once('config/app.php');
 
 // db
-require_once('core/db.php');
+require_once('core/bootstrap.php');  // Eloquent
+require_once('core/db.php');  // PDO
 
 // helpers
 require_once('includes/helpers.php');
-
-// register autoloading of classes
-require_once('vendor/autoload.php');
