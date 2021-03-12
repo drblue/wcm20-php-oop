3d7c58f489f97761f1f7dbdcfa3e8c1e6df47122
Johan Nordström <johan@digitalvillage.se>
2021-03-12 11:40:54 +0100

    Added fillable attributes to Todo model.

--- 12-todoist/app/Models/Todo.php
+++ 12-todoist/app/Models/Todo.php
@@ -6,4 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['title', 'description', 'completed'];
 }
