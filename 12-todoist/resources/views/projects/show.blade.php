2729c8107783fbd26efd559301c9d0b009bdb850
Johan Nordström <johan@digitalvillage.se>
2021-03-15 10:50:30 +0100

    Show a project's todos.

--- 12-todoist/resources/views/projects/show.blade.php
+++ 12-todoist/resources/views/projects/show.blade.php
@@ -3,8 +3,31 @@
 @section('content')
 	<h1 class="mb-3">{{ $project->name }}</h1>
 
-	<h2 class="h5">Todos</h2>
-	<p class="text-muted">Here will be a list of todos for this project.</p>
+	<h2>Todos</h2>
+	<table class="table table-striped table-hover table-responsive">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Title</th>
+				<th>Completed?</th>
+				<th>Created at</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($project->todos as $todo)
+				<tr>
+					<td>{{ $todo->id }}</td>
+					<td>{{ $todo->title }}</td>
+					<td>{{ $todo->completed }}</td>
+					<td>{{ $todo->created_at }}</td>
+					<td>
+						<a href="/todos/{{ $todo->id }}" class="btn btn-primary btn-sm">View</a>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
 
 	<a href="/projects" class="btn btn-secondary">&laquo; Back</a>
 @endsection
--- 12-todoist/resources/views/todos/show.blade.php
+++ 12-todoist/resources/views/todos/show.blade.php
@@ -2,13 +2,36 @@
 
 @section('content')
 	<h1 class="mb-3">{{ $todo->title }}</h1>
+
 	<div class="meta mb-3">
-		<h2 class="h6">Status</h2>
-		@if($todo->completed)
-			Completed 🥳
-		@else
-			Still needs doing 😰
-		@endif
+		<dl class="row">
+			<dt class="col-sm-2">Project</dt>
+			<dd class="col-sm-10">
+				<a href="/projects/{{ $todo->project->id }}">
+					{{ $todo->project->name }}
+				</a>
+			</dd>
+
+			<dt class="col-sm-2">Status</dt>
+			<dd class="col-sm-10">
+				@if($todo->completed)
+					Completed 🥳
+				@else
+					Still needs doing 😰
+				@endif
+			</dd>
+
+			<dt class="col-sm-2">Created at</dt>
+			<dd class="col-sm-10">
+				{{ $todo->created_at }}
+			</dd>
+
+			<dt class="col-sm-2">Last updated at</dt>
+			<dd class="col-sm-10">
+				{{ $todo->updated_at }}
+			</dd>
+
+		</dl>
 	</div>
 
 	@if($todo->description)
@@ -16,5 +39,5 @@
 		<p>{{ $todo->description }}</p>
 	@endif
 
-	<a href="/todos" class="btn btn-secondary">&laquo; Back</a>
+	<a href="/projects/{{ $todo->project->id }}" class="btn btn-secondary">&laquo; Back to the project</a>
 @endsection
