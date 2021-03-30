a2b48db5efc7e86c5389c945b9009cc525a7c0e2
Johan Nordström <johan@digitalvillage.se>
2021-03-30 10:25:27 +0200

    Expose data as JSON in Laravel.

--- 13-blogg/routes/api.php
+++ 13-blogg/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// /api/v1/articles
+Route::resource('v1/articles', ArticleController::class);
